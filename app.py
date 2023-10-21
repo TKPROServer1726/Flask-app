3130c1369754e023fe1e04ae59e6a86d1be42dba
TKPROServer1726 <96683648+TKPROServer1726@users.noreply.github.com>
2023-10-22 03:25:55 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/")
 def fhfjks():
-  return "/"
+  return request.remote_addr
 
-app.run(host='0.0.0.0',port=9999)
+app.run(host='0.0.0.0',port=9927)
